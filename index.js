ac664431aaa17ab9d76d7b981d9fd463786d5816
Huztro <huztroo@gmail.com>
2026-05-21 17:20:33 +0500

    index.js

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const client = new Client({
 });
 
 const PREFIX = '.';
+const OWNER_ID = "1363540480662704248";
 const startTime = Date.now();
 
 // List of blacklisted terminology for the built-in global AutoMod safety engine
