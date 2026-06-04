b7471a7febaf20203b99282d58d85dc4f9ac02e4
Huztro <huztroo@gmail.com>
2026-06-05 03:18:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const { getSettings } = require('./utils/settings.js');
 // =========================
 // PREFIX CONFIG
 // =========================
-const DEFAULT_PREFIX = '!, .';
+const DEFAULT_PREFIX = '!', '.';
 
 function getPrefix(guildId) {
     if (global.prefixes && global.prefixes[guildId]) {
